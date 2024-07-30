b5e9af9f0301429570bac2b421f808b6af5ccd8f
L654456 <359707195@qq.com>
2024-07-30 08:39:47 +0800

    sakana

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,9 @@
 //渲染列表：
 import { TinyColor } from '@ctrl/tinycolor'
 import { Button, ConfigProvider, } from 'antd'
+import React, { useEffect } from 'react'
 import "./css/style.scss"
+import Sakana from 'sakana'
 const colors1 = ['#6253E1', '#04BEFE']
 const getHoverColors = (colors) =>
   colors.map((color) => new TinyColor(color).lighten(5).toString())
@@ -24,8 +26,34 @@ export default function Login () {
       loginButton()
     }
   }
+  useEffect(() => {
+    // 在这里执行你的方法
+    Sakana.setMute(false) // 开启声音
+    Sakana.init({
+      el: '.sakana-box',     // 启动元素 node 或 选择器
+      character: 'takina',          // 启动角色 'chisato','takina' 
+      inertia: 0.08,              // 惯性
+      decay: 0.99,              // 衰减
+      r: 6,                // 启动角度
+      y: 1,                // 启动高度
+      scale: 1,                 // 缩放倍数
+      translateY: 0,                 // 位移高度
+      canSwitchCharacter: true,     // 允许换角色
+      onSwitchCharacter (character) {  // 切换角色回调
+        console.log(`${character} dayo~`)
+      },
+    })
+    return () => { } // 返回一个空函数
+  }, []) // 空数组表示仅在挂载和卸载时执行一次，若有参数，则在参数变化时执行
+
+
+
+
+
   return (
-    <div className="login-body">
+
+    <div className="login-body" style={{ backgroundImage: "url('https://www.loliapi.com/acg/')" }}>
+
       <div className="box">
         <h2>Login</h2>
         <form id="loginForm">
@@ -94,11 +122,17 @@ export default function Login () {
             </div>
           </div>
 
+
         </form>
 
       </div>
 
+      <div className="sakana-box">
+      </div>
+
     </div>
+
   )
+
 }
 
